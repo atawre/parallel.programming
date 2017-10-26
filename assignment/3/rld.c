6d48866eed46dd71af3a986c3f68fe7e9ed97fdf
atawre <atawre@gmail.com>
2017-10-25 23:56:07 -0300

    updated rld

--- assignment/3/rld.c
+++ assignment/3/rld.c
@@ -11,7 +11,7 @@ int prefixSum(int *a, int *b, int n, int id);
 
 int main(int argc, char **argv){
 	struct timespec tstart,tend; 
-  float timer;
+	float timer;
 
 	if(argc < 3) return 1;
 //generate test image (always starts with 0)
@@ -27,26 +27,42 @@ int main(int argc, char **argv){
 		image[i] = size;
 		//printf("%d ", size);
 	}
-	//printf("\n");
 	char *uncompressed = malloc(count*sizeof(char));
 	clock_gettime(CLOCK_MONOTONIC, &tstart);
 	int nt = omp_get_max_threads();
 	int *b = malloc(nt*sizeof(int));
 	int *bc = malloc(nt*sizeof(int));
-	
+
+for(int k=0; k<numRepeats; k++)
+	printf("%d ", image[k]);
+printf("\n");
 	#pragma omp parallel
 	{
 		int id = omp_get_thread_num();
+
+//prefixSum(image, imagePsum, numRepeats, id);
+
 		prefixSumCoarse(image, imagePsum, b, bc, numRepeats, id, nt);
-		#pragma omp for
-		for(int i=0; i<numRepeats; i++)
-			for(int j=0; j<image[i]; j++)
-				uncompressed[imagePsum[i]+ j] = i&1;
+		//#pragma omp for
+		for(int i=0; i<numRepeats; i++){
+			for(int j=0; j<image[i]; j++){
+				//printf("(%d, %d) ", i,j);
+				//uncompressed[imagePsum[i]+ j] = i&1;
+			}
+			//printf("\n");
+		}
 	}
 	clock_gettime(CLOCK_MONOTONIC, &tend);
-    timer = (tend.tv_sec-tstart.tv_sec) +
+	timer = (tend.tv_sec-tstart.tv_sec) +
         (tend.tv_nsec-tstart.tv_nsec)*1.0e-9;
-	printf("parallel runtime = %f\n", timer);
+	//printf("parallel runtime = %f\n", timer);
+
+for(int k=0; k<numRepeats; k++)
+	printf("%d ", imagePsum[k]);
+printf("\n");
+for(int k=0; k<nt; k++)
+	printf("%d ", b[k]);
+printf("\n");
 
 	int k = 0;
 	int passed = 1;
@@ -54,7 +70,7 @@ int main(int argc, char **argv){
 		for(int j=0; j<image[i]; j++){
 			int colour = i&1;
 			if(colour != uncompressed[k]){ 
-				printf("k: %d vs %d\n", k, colour, uncompressed[k]);
+				//printf("k: %d vs %d\n", k, colour, uncompressed[k]);
 				passed = 0;
 			}
 			k++;
@@ -62,10 +78,42 @@ int main(int argc, char **argv){
 	}
 	if(passed)
 		printf("result correct\n");
+	printf("\n");
 	return 0;
 }
+/*
+// assumes n divisible by p
+// shared a, b
+// start ← id ∗ n/p
+// sum ← 0
+// for j ← 0 to n/p − 1 do
+// 	sum ← sum + a[start + j]
+// 	a[start + j] ← sum
+// end
+// b[id] ← a[(id + 1) ∗ n/p − 1] // sum of all values in sub-array
+// BARRIER
+// scan(b)
+// if id > 0 then
+// 	for j ← 0 to n/p − 1 do
+//		a[start + j] ← a[start + j] + b[id − 1]
+// 	end
+// end
+ */
 
 void prefixSumCoarse(int *a, int *as, int *b, int *bc, int n, int id, int nt){
+	//printf("id: %d ", id);
+	int sum=0;
+	int chunk = n/nt;
+	int start=id*chunk;
+
+	//printf("id=%d => chunk : %d, start : %d\n",id, chunk, start);
+	for(int j=0;j<chunk;j++){
+		sum += a[start+j];
+		as[start+j] = sum;
+	}
+	b[id]=as[(id+1)*chunk - 1];
+	#pragma omp barrier
+	prefixSum(b, bc, nt, id);
 
 }
 
